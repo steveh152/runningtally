9f8f602e638fa0db91d6bb83eed655fa44215ca8
Steve Hammar <steveh152@gmail.com>
2013-07-15 21:17:43 -0400

    SQLite stuff almost there

--- RunningTally/src/main/java/com/RCOS/runningtally/Comment.java
+++
@@ -1,7 +0,0 @@
-package com.RCOS.runningtally;
-
-/**
- * Created by steve on 6/30/13.
- */
-public class Comment {
-}
---
+++ RunningTally/src/main/java/com/RCOS/runningtally/DAO.java
@@ -0,0 +1,7 @@
+package com.RCOS.runningtally;
+
+/**
+ * Created by steve on 7/13/13.
+ */
+public class DAO {
+}
